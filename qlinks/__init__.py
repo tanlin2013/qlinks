e0825bb6c0cadd2daa7f80ab8879ed90b7939a1e
taolin <tanlin2013@gmail.com>
2023-02-01 22:32:07 +0800

    feat: set version and logger

--- qlinks/__init__.py
+++ qlinks/__init__.py
@@ -1,5 +1,21 @@
-"""**qlinks**
+import logging
+from importlib import metadata
 
-Quantum link model
-"""
-__version__ = "0.0.1"
+
+# -- Version -----------------------------------------------------------------
+
+__version__ = metadata.version(__name__)
+
+# -- Define logger and the associated formatter and handler ------------------
+
+formatter = logging.Formatter(
+    "%(asctime)s [%(filename)s] %(levelname)s: %(message)s", datefmt="%Y-%m-%d %H:%M:%S"
+)
+
+handler = logging.StreamHandler()
+handler.setLevel(logging.INFO)
+handler.setFormatter(formatter)
+
+logger = logging.getLogger("qlinks")
+logger.setLevel(logging.INFO)
+logger.addHandler(handler)
