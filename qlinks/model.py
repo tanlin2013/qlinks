24fdce0890e1753117736a899f1a16915a48d007
taolin <tanlin2013@gmail.com>
2023-02-04 14:43:54 +0800

    feat: quantum link model hamiltonian

---
+++ qlinks/model.py
@@ -0,0 +1,28 @@
+from __future__ import annotations
+
+from dataclasses import dataclass, field
+
+import numpy as np
+
+from qlinks.spin_object import SpinOperator
+from qlinks.square_lattice import SquareLattice
+
+
+@dataclass
+class QuantumLinkModel(SquareLattice):
+    coup_j: float
+    coup_rk: float
+    _hamiltonian: SpinOperator = field(init=False, repr=False)
+
+    def __post_init__(self):
+        self._hamiltonian = SpinOperator(np.zeros(self.hilbert_dims))
+        for plaquette in self.iter_plaquettes():
+            flipper = plaquette + plaquette.conj()
+            self._hamiltonian += -self.coup_j * flipper + self.coup_rk * flipper ** 2
+
+    @property
+    def hamiltonian(self) -> SpinOperator:
+        return self._hamiltonian
+
+    def gen_config(self):
+        pass
---
+++ tests/test_model.py
@@ -0,0 +1,12 @@
+from matplotlib import pyplot as plt
+
+from qlinks.model import QuantumLinkModel
+
+
+class TestQuantumLinkModel:
+
+    def test_hamiltonian(self):
+        model = QuantumLinkModel(2, 2, 1, 1)
+        ham = model.hamiltonian
+        plt.matshow(ham)
+        plt.show()
