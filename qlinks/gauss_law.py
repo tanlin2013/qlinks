0e854c8c98391b391b13b61bca58b93d30ac540b
taolin <tanlin2013@gmail.com>
2023-02-06 23:34:32 +0800

    feat: gen config from gauss law

--- qlinks/gauss_law.py
+++ qlinks/gauss_law.py
@@ -1,6 +1,101 @@
-import numpy as np  # noqa: F401
+from __future__ import annotations
+
+from copy import deepcopy
+from dataclasses import astuple, dataclass, field
+from enum import IntEnum
+from itertools import product
+from typing import Tuple, List, Dict, Optional
+
+import numpy as np
+from numpy.typing import ArrayLike
+
+from qlinks.exceptions import LinkOverridingError
+from qlinks.lattice.component import Site, UnitVectors
+from qlinks.lattice.square_lattice import SquareLattice
+from qlinks.solver.deep_first_search import Node
+from qlinks.spin_object import Spin, SpinConfigs
 
 
+class Flow(IntEnum):
+    outward = 1
+    inward = -1
+
+
+@dataclass
 class GaussLaw:
-    def __init__(self):
+    charge: int = 0
+    __hash_table: Dict[int, Spin] = field(init=False, repr=False)
+
+    def __post_init__(self):
+        if abs(self.charge) > 2:
+            raise ValueError("Charge ranges from -2 to +2.")
+        self.__hash_table = {spin.magnetization: spin for spin in SpinConfigs}
+
+    def possible_flows(self) -> List[Tuple[int, ...]]:
+        flows = product([flow.value for flow in Flow], repeat=4)
+        return [quartet for quartet in flows if sum(quartet) / 2 == self.charge]
+
+    def possible_configs(self) -> List[Tuple[Spin, ...]]:
+        local_coord_sys = [unit_vec.sign for unit_vec in UnitVectors.iter_all_directions()]
+        return [
+            tuple(map(lambda idx: self.__hash_table[idx], np.multiply(quartet, local_coord_sys)))
+            for quartet in self.possible_flows()
+        ]
+
+
+@dataclass
+class SpinConfigSnapshot(Node, SquareLattice):
+    charge_distri: Optional[ArrayLike | np.ndarray] = None
+
+    def __post_init__(self):
+        super().__post_init__()
+        if self.charge_distri is None:
+            self.charge_distri = np.zeros(self.shape)
+        self.charge_distri = np.flipud(self.charge_distri).T
+        assert self.charge_distri.shape == self.shape
+
+    def __eq__(self, other: object) -> bool:
+        if not isinstance(other, SpinConfigSnapshot):
+            raise NotImplemented
+        assert self.shape == other.shape
+        return set(self._links.values()) == set(other._links.values())
+
+    def __str__(self) -> str:
+        return ",\n".join(map(repr, list(self.links.values())))
+
+    def find_first_empty_site(self) -> Site | None:  # type: ignore[return]
+        for site in self:
+            if np.isnan(self.charge(site)):
+                return site
+        raise StopIteration("No empty site found, all links have been set.")
+
+    def extend_node(self) -> List[SpinConfigSnapshot]:
+        site = self.find_first_empty_site()
+        charge = self.charge_distri[*astuple(site)]
+        new_nodes = []
+        for config in GaussLaw(charge).possible_configs():
+            try:
+                new_node = deepcopy(self)
+                new_node.set_cross(site, config)
+                new_nodes.append(new_node)
+            except LinkOverridingError:
+                continue
+        return new_nodes
+
+    def is_the_solution(self) -> bool:
+        for site in self:
+            if np.isnan(self.charge(site)):
+                return False
+        return True
+
+    @property
+    def links(self):
+        assert self.is_the_solution()
+        return self._links
+
+    @property
+    def adjacency_matrix(self):
+        return
+
+    def plot(self):
         pass
---
+++ tests/test_gauss_law.py
@@ -0,0 +1,38 @@
+import pytest
+import numpy as np
+from dataclasses import astuple
+
+from qlinks.gauss_law import GaussLaw, SpinConfigSnapshot
+from qlinks.solver.deep_first_search import DeepFirstSearch
+
+
+class TestGaussLaw:
+    @pytest.mark.parametrize("charge, n_config", [(-2, 1), (-1, 4), (0, 6), (1, 4), (2, 1)])
+    def test_possible_flows(self, charge, n_config):
+        assert len(GaussLaw(charge).possible_flows()) == n_config
+
+    @pytest.mark.parametrize("charge", [-2, -1, 0, 1, 2])
+    def test_possible_configs(self, charge):
+        configs = GaussLaw(charge).possible_configs()
+        for config in configs:
+            mag = list(map(lambda spin: spin.magnetization, config))
+            assert np.sum(np.multiply(mag, [1, 1, -1, -1])) / 2 == charge
+
+
+class TestSpinConfigSnapshot:
+    @pytest.mark.parametrize("length, width", [(2, 2)])
+    def test_extend_node(self, length, width):
+        charge_distri = np.zeros((length, width))
+        snapshot = SpinConfigSnapshot(length, width, charge_distri)
+        for snap in snapshot.extend_node():
+            print(snap.links)
+
+    @pytest.mark.parametrize("charge_distri",
+                             [np.zeros((2, 2)), [[1, 0], [-1, 0]], [[-2, 0], [0, 2]]])
+    def test_search(self, charge_distri):
+        width, length = np.asarray(charge_distri).shape
+        snapshot = SpinConfigSnapshot(length, width, charge_distri)
+        dfs = DeepFirstSearch(snapshot)
+        filled_snapshot = dfs.search()
+        for site in filled_snapshot:
+            assert filled_snapshot.charge(site) == filled_snapshot.charge_distri[*astuple(site)]
