bbcfb89cb9f92d545a59fb7b4ea118ee821bdef2
taolin <tanlin2013@gmail.com>
2023-02-07 15:23:20 +0800

    test: model ham is hermitian

--- tests/test_model.py
+++ tests/test_model.py
@@ -1,11 +1,18 @@
+import pytest
 from matplotlib import pyplot as plt
+from scipy.linalg import ishermitian
 
 from qlinks.model import QuantumLinkModel
 
 
 class TestQuantumLinkModel:
-    def test_hamiltonian(self):
-        model = QuantumLinkModel(2, 2, 1, 1)
+    @pytest.mark.parametrize("length, width", [(2, 2)])
+    @pytest.mark.parametrize("coup_j, coup_rk", [(10, 1), (1, -10)])
+    def test_hamiltonian(self, length, width, coup_j, coup_rk):
+        model = QuantumLinkModel(length, width, coup_j, coup_rk)
         ham = model.hamiltonian
+        assert ishermitian(ham)
+        # assert np.all(eigh(ham, eigvals_only=True) >= -1e-12)  # positive semi-definite
         plt.matshow(ham)
+        plt.colorbar()
         plt.show()
