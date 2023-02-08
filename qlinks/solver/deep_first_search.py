82e47c55ba6180a64115ba8ee0210dce648eddea
taolin <tanlin2013@gmail.com>
2023-02-09 00:55:02 +0800

    docs: add doc for dfs

--- qlinks/solver/deep_first_search.py
+++ qlinks/solver/deep_first_search.py
@@ -31,6 +31,29 @@ class Node(abc.ABC):
 
 @dataclass
 class DeepFirstSearch:
+    """Deep first search (DFS) algorithm.
+
+    Args:
+        start_state: The initial state as the first :class:`Node`.
+        max_steps: The maximum number of steps for deep first search. Default 1000.
+
+    Attributes:
+        frontier: A :class:`deque` of :class:`Node`.
+        checked_nodes: A list of checked :class:`Node`.
+
+    Examples:
+        If :class:`Node` has been implemented, the algorithm can be launched through
+
+        >>> init_state: AnyNode = Node()  # any child class of Node
+        >>> dfs = DeepFirstSearch(init_state)
+        >>> selected_state: AnyNode = dfs.search()
+
+    Notes:
+        For general purpose, users must implement all abstractive methods in :class:`Node`.
+
+    References:
+        https://python.plainenglish.io/solve-sudoku-using-depth-first-search-algorithm-dfs-in-python-2be3caa08ccd
+    """
     start_state: AnyNode
     max_steps: int = 1000
     frontier: Deque[AnyNode] = field(default_factory=deque)
@@ -50,7 +73,16 @@ class DeepFirstSearch:
     def frontier_is_empty(self) -> bool:
         return True if len(self.frontier) == 0 else False
 
-    def search(self) -> AnyNode | None:  # type: ignore[return]
+    def search(self) -> AnyNode:  # type: ignore[return]
+        """Search for the solution.
+
+        Returns:
+            A :class:`Node` which fulfills :func:`~Node.is_the_solution`.
+
+        Raises:
+            StopIteration: if no solution can be found or the number of iteration
+             exceeds `max_steps`.
+        """
         for n_step in count(start=1):
             if self.frontier_is_empty() or n_step == self.max_steps:
                 raise StopIteration(f"No Solution Found after {n_step} steps!")
@@ -60,7 +92,7 @@ class DeepFirstSearch:
             if selected_node.is_the_solution():
                 logger.info(f"Solution Found in {n_step} steps.")
                 logger.info(f"\n{selected_node}")
-                return selected_node
+                return selected_node  # TODO: Do I need to find all possibilities, or just one?
 
             new_nodes = selected_node.extend_node()
 
