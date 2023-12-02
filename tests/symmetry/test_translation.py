0cfdb59ed4503a8266ffa3c4c1e77750d9ee897f
taolin <tanlin2013@gmail.com>
2023-12-03 00:02:19 +0800

    test: add fixture for translation

--- tests/symmetry/test_translation.py
+++ tests/symmetry/test_translation.py
@@ -1,26 +1,36 @@
-import pytest  # noqa: F401
+import pytest
+import numpy as np
 
-from qlinks.lattice.component import UnitVector
+from qlinks.lattice.square_lattice import SquareLattice
+from qlinks.symmetry.computation_basis import ComputationBasis
 from qlinks.symmetry.translation import Translation
-from tests.lattice.test_square_lattice import (  # noqa: F401
-    anti_clockwise_state,
-    clockwise_state,
-    zero_clock_state,
-)
 
 
 class TestTranslation:
-    def test_matrix_multiplication(
-        self, clockwise_state, anti_clockwise_state, zero_clock_state  # noqa: F811
-    ):
-        for state in [clockwise_state, anti_clockwise_state, zero_clock_state]:
-            # translate by lattice size
-            assert Translation(UnitVector(0, state.shape[1])) @ state == state
-            assert Translation(UnitVector(state.shape[0], 0)) @ state == state
-            assert Translation(UnitVector(*state.shape)) @ state == state
-            # translate back and forth in x
-            assert Translation(UnitVector(1, 0)) @ state != state
-            assert Translation(UnitVector(-1, 0)) @ (Translation(UnitVector(1, 0)) @ state) == state
-            # translate back and forth in y
-            assert Translation(UnitVector(0, 1)) @ state != state
-            assert Translation(UnitVector(0, -1)) @ (Translation(UnitVector(0, 1)) @ state) == state
+    @pytest.fixture(scope="function")
+    def lattice(self):
+        return SquareLattice(2, 2)
+
+    @pytest.fixture(
+        scope="function",
+        params=[
+            np.array(
+                [
+                    [0, 0, 0, 1, 1, 0, 1, 1],
+                    [0, 1, 0, 0, 1, 1, 1, 0],
+                    [0, 1, 1, 0, 1, 0, 0, 1],
+                    [1, 0, 0, 1, 0, 1, 1, 0],
+                    [1, 0, 1, 1, 0, 0, 0, 1],
+                    [1, 1, 1, 0, 0, 1, 0, 0],
+                ]
+            )
+        ],
+    )
+    def basis(self, request):
+        basis = ComputationBasis(request.param)
+        basis.sort()
+        return basis
+
+    def test(self, lattice, basis):
+        translation = Translation(lattice, basis)
+        df = translation._df
