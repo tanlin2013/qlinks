731589af07aaa2b9df468c38524f9f101bab838d
taolin <tanlin2013@gmail.com>
2023-12-03 00:01:12 +0800

    test: mat elems of ham and evals

--- tests/test_model.py
+++ tests/test_model.py
@@ -1,78 +1,43 @@
-import networkx as nx
 import numpy as np
 import pytest
-from matplotlib import pyplot as plt
 from scipy.linalg import eigh, ishermitian
 
 from qlinks.model import QuantumLinkModel
+from qlinks.solver.deep_first_search import DeepFirstSearch
 from qlinks.symmetry.computation_basis import ComputationBasis
 from qlinks.symmetry.gauss_law import GaussLaw
 
 
 class TestQuantumLinkModel:
-    @pytest.mark.parametrize("length, width", [(2, 2)])
-    @pytest.mark.parametrize("coup_j, coup_rk", [(10, 1), (1, -10)])
-    def test_hamiltonian(self, length, width, coup_j, coup_rk):
-        model = QuantumLinkModel(length, width, coup_j, coup_rk)
+    @pytest.mark.parametrize("coup_j, coup_rk", [(1, 1), (1, 0.1)])
+    @pytest.mark.parametrize("length_x, length_y", [(2, 2)])
+    @pytest.mark.parametrize("basis", [
+        ComputationBasis(np.array([
+            [0, 0, 0, 1, 1, 0, 1, 1], [0, 1, 0, 0, 1, 1, 1, 0], [0, 1, 1, 0, 1, 0, 0, 1],
+            [1, 0, 0, 1, 0, 1, 1, 0], [1, 0, 1, 1, 0, 0, 0, 1], [1, 1, 1, 0, 0, 1, 0, 0]]
+        ))
+    ])
+    def test_hamiltonian(self, coup_j, coup_rk, length_x, length_y, basis):
+        assert basis.n_links == 2 * length_x * length_y
+        model = QuantumLinkModel(coup_j, coup_rk, (length_x, length_y), basis)
         ham = model.hamiltonian
         assert ishermitian(ham)
-        evals = eigh(ham, eigvals_only=True)
-        print(evals)
-        plt.matshow(ham)
-        plt.colorbar()
-        plt.show()
+        evals, evecs = eigh(ham)
+        if np.isclose(coup_rk / coup_j, 1):
+            assert np.isclose(evals[0], 0)  # E0 = 0, when coup_rk / coup_j = 1
 
-        graph = nx.from_numpy_array(ham)
-        edges, weights = zip(*nx.get_edge_attributes(graph, "weight").items())
-        nx.draw(
-            graph,
-            # pos=nx.circular_layout(graph),
-            with_labels=True,
-            edgelist=edges,
-            edge_color=weights,
-            edge_cmap=plt.cm.jet,
-        )
-        plt.show()
+    @pytest.fixture(scope="class")
+    def basis_from_solver(self):
+        gauss_law = GaussLaw.from_zero_charge_distri(6, 4)
+        gauss_law.flux_sector = (0, 0)
+        dfs = DeepFirstSearch(gauss_law, max_steps=int(1e+8))
+        return gauss_law.to_basis(dfs.solve(n_solution=32810))  # 10 secs
 
-    @pytest.mark.parametrize(
-        "charge_distri, flux_sector",
-        [
-            (GaussLaw.staggered_charge_distri(2, 2), (0, 0)),
-            (GaussLaw.staggered_charge_distri(4, 4), (0, 0)),  # 26 secs
-            # (GaussLaw.staggered_charge_distri(4, 4), None),  # 1 mins 20 secs
-            (np.zeros((2, 2)), None),
-            # (np.zeros((4, 4)), (0, 0)),  # 9 mins 26 secs
-        ],
-    )
-    @pytest.mark.parametrize("coup_j, coup_rk", [(1, 0.9)])
-    def test_symmetry_sector_hamiltonian(self, charge_distri, flux_sector, coup_j, coup_rk):
-        length, width = charge_distri.shape
-        basis = ComputationBasis(length, width, charge_distri, flux_sector).get()
-        model = QuantumLinkModel(length, width, coup_j, coup_rk, basis)
+    @pytest.mark.parametrize("coup_j, coup_rk", [(1, 1)])
+    def test_with_solver(self, coup_j, coup_rk, basis_from_solver):
+        model = QuantumLinkModel(coup_j, coup_rk, (6, 4), basis_from_solver)
         ham = model.hamiltonian
         assert ishermitian(ham)
-        evals = eigh(ham, eigvals_only=True)  # ground state energy = 0, when coup_rk = 1
-        print(evals)
-        plt.matshow(ham)
-        plt.colorbar()
-        plt.show()
-
-        diag = np.diagonal(ham)
-        plt.hist(diag, np.linspace(np.min(diag), np.max(diag), 20))
-        plt.show()
-
-        plt.hist(evals, np.linspace(np.min(evals), np.max(evals), 100))
-        plt.show()
-
-        graph = nx.from_numpy_array(ham)
-        edges, weights = zip(*nx.get_edge_attributes(graph, "weight").items())
-        nx.draw(
-            graph,
-            pos=nx.spectral_layout(graph),
-            with_labels=True,
-            edgelist=edges,
-            edge_color=weights,
-            edge_cmap=plt.cm.jet,
-        )
-        plt.show()
-        print([len(c) for c in nx.connected_components(graph)])
+        evals, evecs = eigh(ham)
+        if np.isclose(coup_rk / coup_j, 1):
+            assert np.isclose(evals[0], 0)  # E0 = 0, when coup_rk / coup_j = 1
