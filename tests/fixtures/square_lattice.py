2dfc8a996acaaa6c6d27b14ed543c0addaac78c8
taolin <tanlin2013@gmail.com>
2023-12-04 17:53:09 +0800

    docs: update ascii plot

--- tests/fixtures/square_lattice.py
+++ tests/fixtures/square_lattice.py
@@ -12,13 +12,14 @@ def empty_2x2_lattice():
 @pytest.fixture(scope="function")
 def preset_2x2_lattice():
     """
-       │      │
-       ▼      ▼
-    ──►o◄─────o──►
-       ▲      ▲
-       │      │
-    ──►o◄─────o──►
-       │      │
-       ▼      ▼
+    o◄──────o──────►o
+    │       │       │
+    │       │       │
+    ▼       ▼       ▼
+    o◄──────o──────►o
+    ▲       ▲       ▲
+    │       │       │
+    │       │       │
+    o◄──────o──────►o
     """
     return SquareLattice(2, 2, np.array([0, 1, 1, 1, 0, 0, 1, 0]))
