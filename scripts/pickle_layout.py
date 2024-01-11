f60d2f560dcff8dfc46c54f5c3f256e0e6190c30
taolin <tanlin2013@gmail.com>
2024-01-11 16:35:58 +0800

    chore: add job script

---
+++ scripts/pickle_layout.py
@@ -0,0 +1,20 @@
+import pickle
+
+import networkx as nx
+import igraph
+
+from ed import *
+
+
+if __name__ == "__main__":
+    coup_j, coup_rk = (1, -0.7)  # dfs 3 mins 33 secs
+    basis, model = setup_link_model(
+        lattice_shape=(6, 4), n_solution=32810, coup_j=coup_j, coup_rk=coup_rk
+    )
+
+    g = nx.from_numpy_array(-model.kinetic_term)
+    ig = igraph.Graph.from_networkx(g)
+    ig.vs["label"] = [str(i) for i in range(ig.vcount())]
+    layout = ig.layout_kamada_kawai()
+
+    pickle.dumps(layout)
