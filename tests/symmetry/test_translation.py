7f1b7e1fac434d6c10ec722127dcb230de250ab7
taolin <tanlin2013@gmail.com>
2024-04-17 00:43:48 +0800

    refactor: lattice size as fixture params

--- tests/symmetry/test_translation.py
+++ tests/symmetry/test_translation.py
@@ -30,26 +30,73 @@ class TestTranslation:
     """
 
     @pytest.fixture(scope="function")
-    def lattice(self):
-        return SquareLattice(2, 2)
+    def lattice(self, request):
+        return SquareLattice(*request.param)
 
-    def test_df(self, lattice, lattice_2x2_basis):
-        translation = Translation(lattice, lattice_2x2_basis)
-        pd.testing.assert_frame_equal(
-            translation._df,
-            pd.DataFrame(
-                np.array(
-                    [
-                        [27, 78, 105, 150, 177, 228],
-                        [177, 228, 150, 105, 27, 78],
-                        [78, 27, 150, 105, 228, 177],
-                        [228, 177, 105, 150, 78, 27],
-                    ]
+    @pytest.fixture(scope="function")
+    def translation(self, request) -> Translation:
+        model, size = request.param
+        fixture = f"{model}_{'x'.join(map(str, size))}_basis"
+        basis = request.getfixturevalue(fixture)
+        return Translation(SquareLattice(*size), basis)
+
+    @pytest.mark.parametrize(
+        "translation, expected",
+        [
+            (
+                ("qlm", (2, 2)),
+                pd.DataFrame(
+                    np.array(
+                        [
+                            [27, 78, 105, 150, 177, 228],
+                            [177, 228, 150, 105, 27, 78],
+                            [78, 27, 150, 105, 228, 177],
+                            [228, 177, 105, 150, 78, 27],
+                        ]
+                    ),
+                    columns=np.asarray([27, 78, 105, 150, 177, 228], dtype=object),
+                    index=[(0, 0), (0, 1), (1, 0), (1, 1)],
                 ),
-                columns=np.asarray([27, 78, 105, 150, 177, 228], dtype=object),
-                index=[(0, 0), (0, 1), (1, 0), (1, 1)],
             ),
-        )
+            (
+                ("qdm", (4, 2)),
+                # fmt: off
+                pd.DataFrame(
+                    np.array(
+                        [
+                            [17595, 18867, 19638, 36135, 37947, 39219, 39990, 44307,
+                             50283, 51555, 52326, 52773, 55410, 55857, 60498, 60945],
+                            [47940, 45897, 46668, 10125, 15252, 13209, 13980, 5037,
+                             27588, 25545, 26316, 9678, 29400, 12762, 21228, 4590],
+                            [4590, 9678, 12762, 13980, 21228, 26316, 29400, 46668,
+                             5037, 10125, 13209, 15252, 25545, 27588, 45897, 47940],
+                            [60945, 52773, 55857, 39990, 60498, 52326, 55410, 19638,
+                             44307, 36135, 39219, 37947, 51555, 50283, 18867, 17595],
+                            [17595, 37947, 50283, 55410, 18867, 39219, 51555, 55857,
+                             19638, 39990, 52326, 60498, 36135, 44307, 52773, 60945],
+                            [47940, 15252, 27588, 29400, 45897, 13209, 25545, 12762,
+                             46668, 13980, 26316, 21228, 10125, 5037, 9678, 4590],
+                            [4590, 21228, 5037, 25545, 9678, 26316, 10125, 27588,
+                             12762, 29400, 13209, 45897, 13980, 46668, 15252, 47940],
+                            [60945, 60498, 44307, 51555, 52773, 52326, 36135, 50283,
+                             55857, 55410, 39219, 18867, 39990, 19638, 37947, 17595],
+                        ]
+                    ),
+                    columns=np.asarray(
+                        [17595, 18867, 19638, 36135, 37947, 39219, 39990, 44307, 50283,
+                         51555, 52326, 52773, 55410, 55857, 60498, 60945],
+                        dtype=object,
+                    ),
+                    index=[(0, 0), (0, 1), (1, 0), (1, 1), (2, 0), (2, 1), (3, 0), (3, 1)],
+                ),
+                # fmt: on
+            ),
+        ],
+        indirect=["translation"],
+    )
+    def test_df(self, translation, expected):
+        pd.options.display.max_columns = None
+        pd.testing.assert_frame_equal(translation._df, expected)
 
     def test_representative(self, lattice, lattice_2x2_basis):
         translation = Translation(lattice, lattice_2x2_basis)
