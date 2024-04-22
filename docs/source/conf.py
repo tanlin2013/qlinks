171a05b176355b4e3054b034356f83a8eabc3acb
taolin <tanlin2013@gmail.com>
2024-04-22 16:36:06 +0800

    build: exclude notebook from docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -60,7 +60,7 @@ source_suffix = [".rst", ".md"]
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = []
+exclude_patterns = [".ipynb"]
 
 # Allowing docstring in both __init__ and right under class definition
 autoclass_content = "both"
