20ee0ce95578b35c4645a2a078c549ed3e12f83b
taolin <tanlin2013@gmail.com>
2024-11-22 23:59:45 +0800

    feat: bipartite parity operator

--- qlinks/model/spin1_xy_model_1d.py
+++ qlinks/model/spin1_xy_model_1d.py
@@ -5,6 +5,7 @@ from functools import reduce
 from itertools import product
 
 import numpy as np
+import numpy.typing as npt
 import pandas as pd
 import scipy.sparse as sp
 
@@ -23,15 +24,17 @@ class Spin1XYModel:
     _potential_term: sp.sparray = field(init=False, repr=False)
     _potential_term2: sp.sparray = field(init=False, repr=False)
     _hamiltonian: sp.sparray = field(init=False, repr=False)
+    _parity: npt.NDArray = field(init=False, repr=False)
 
     def __post_init__(self):
         sop = SpinOperators(1)
         s_plus, s_minus, s_z = sop.s_plus, sop.s_minus, sop.s_z
+        sz_str = np.array([[-1, 0, 0], [0, 1, 0], [0, 0, -1]], dtype=int)
 
         self._kinetic_term = sp.csr_array((3**self.n, 3**self.n), dtype=float)
         self._potential_term = sp.csr_array((3**self.n, 3**self.n), dtype=float)
         self._potential_term2 = sp.csr_array((3**self.n, 3**self.n), dtype=float)
-        self._hamiltonian = sp.csr_array((3**self.n, 3**self.n), dtype=float)
+        self._parity = np.eye(3**self.n, dtype=int)
 
         for site in range(self.n):
             self._kinetic_term += 0.5 * kron([s_plus, s_minus, *[np.eye(3)] * (self.n - 2)], shift=site)
@@ -47,7 +50,12 @@ class Spin1XYModel:
                 sp.kron, [sp.eye(3**site), s_z**2, sp.eye(3 ** (self.n - 1 - site))]
             )
 
-        self._hamiltonian += (
+        for site in range(0, self.n, 2):
+            self._parity @= reduce(
+                np.kron, [np.eye(3**site, dtype=int), sz_str, np.eye(3 ** (self.n - 1 - site), dtype=int)]
+            )
+
+        self._hamiltonian = (
             self.coup_j * self._kinetic_term
             + self.coup_h * self._potential_term
             + self.coup_d * self._potential_term2
@@ -68,6 +76,10 @@ class Spin1XYModel:
     @property
     def hamiltonian(self) -> sp.sparray:
         return self._hamiltonian
+
+    @property
+    def parity(self) -> npt.NDArray[np.int64]:
+        return self._parity
 
     @property
     def basis(self) -> pd.DataFrame:
