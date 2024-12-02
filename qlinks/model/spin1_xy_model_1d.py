5ce7dfb9c566d26a27a276ad5df821825ef2672a
taolin <tanlin2013@gmail.com>
2024-12-02 17:59:45 +0800

    fix: kin and pot should be defined as diag and off-diag of H

--- qlinks/model/spin1_xy_model_1d.py
+++ qlinks/model/spin1_xy_model_1d.py
@@ -10,7 +10,7 @@ import scipy.sparse as sp
 from scipy.linalg import expm
 
 from qlinks.lattice.spin_operators import SpinOperators
-from qlinks.model.utils import kron
+from qlinks.model.utils import kron, sparse_real_if_close
 
 
 @dataclass(slots=True)
@@ -23,53 +23,49 @@ class Spin1XYModel:
     periodic: bool = False
     _kinetic_term: sp.sparray = field(init=False, repr=False)
     _potential_term: sp.sparray = field(init=False, repr=False)
-    _potential_term2: sp.sparray = field(init=False, repr=False)
-    _h3_term: sp.sparray = field(init=False, repr=False)
     _hamiltonian: sp.sparray = field(init=False, repr=False)
     _parity: npt.NDArray = field(init=False, repr=False)
 
     def __post_init__(self):
-        self._kinetic_term = sp.csr_array((3**self.n, 3**self.n), dtype=float)
-        self._potential_term = sp.csr_array((3**self.n, 3**self.n), dtype=float)
-        self._potential_term2 = sp.csr_array((3**self.n, 3**self.n), dtype=float)
-        self._h3_term = sp.csr_array((3**self.n, 3**self.n), dtype=float)
+        self._hamiltonian = sp.csr_array((3**self.n, 3**self.n), dtype=float)
         self._parity = np.eye(3**self.n, dtype=int)
 
-        self._build_kinetic_term()
-        self._build_potential_term()
+        self._build_xy_term()
+        self._build_sz_term()
         self._build_h3_term()
+        self._hamiltonian = sparse_real_if_close(self._hamiltonian)
+        self._potential_term = sp.diags(self._hamiltonian.diagonal()).tocsr()
+        self._kinetic_term = self._hamiltonian - self._potential_term
         self._build_parity_operator()
-        self._hamiltonian = (
-            self.coup_j * self._kinetic_term
-            + self.coup_h * self._potential_term
-            + self.coup_d * self._potential_term2
-            + self.coup_j3 * self._h3_term
-        )
 
-    def _build_kinetic_term(self):
+    def _build_xy_term(self):
         sop = SpinOperators(1)
         s_plus, s_minus, idty = sop.s_plus, sop.s_minus, sop.idty
         for site in range(self.n):
-            self._kinetic_term += 0.5 * kron([s_plus, s_minus, *[idty] * (self.n - 2)], shift=site)
-            self._kinetic_term += 0.5 * kron([s_minus, s_plus, *[idty] * (self.n - 2)], shift=site)
+            self._hamiltonian += (
+                self.coup_j * 0.5 * kron([s_plus, s_minus, *[idty] * (self.n - 2)], shift=site)
+            )
+            self._hamiltonian += (
+                self.coup_j * 0.5 * kron([s_minus, s_plus, *[idty] * (self.n - 2)], shift=site)
+            )
             if not self.periodic and site == self.n - 2:
                 break
 
-    def _build_potential_term(self):
+    def _build_sz_term(self):
         sop = SpinOperators(1)
         s_z, idty = sop.s_z, sop.idty
         for site in range(self.n):
-            self._potential_term += kron([s_z, *[idty] * (self.n - 1)], shift=site)
-            self._potential_term2 += kron([s_z**2, *[idty] * (self.n - 1)], shift=site)
+            self._hamiltonian += self.coup_h * kron([s_z, *[idty] * (self.n - 1)], shift=site)
+            self._hamiltonian += self.coup_d * kron([s_z**2, *[idty] * (self.n - 1)], shift=site)
 
     def _build_h3_term(self):
         sop = SpinOperators(1)
         s_x, s_y, idty = sop.s_x, sop.s_y, sop.idty
         for site in range(self.n):
-            self._h3_term += kron(
+            self._hamiltonian += self.coup_j3 * kron(
                 [s_x, idty, idty, s_x, *[idty] * (self.n - 4)], shift=site
             )
-            self._h3_term += kron(
+            self._hamiltonian += self.coup_j3 * kron(
                 [s_y, idty, idty, s_y, *[idty] * (self.n - 4)], shift=site
             )
             if not self.periodic and site == self.n - 4:
@@ -91,14 +87,6 @@ class Spin1XYModel:
     @property
     def potential_term(self) -> sp.sparray:
         return self._potential_term
-
-    @property
-    def potential_term2(self) -> sp.sparray:
-        return self._potential_term2
-
-    @property
-    def h3_term(self) -> sp.sparray:
-        return self._h3_term
 
     @property
     def hamiltonian(self) -> sp.sparray:
