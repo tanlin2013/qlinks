b9a57300c926286e3b247ee9d4795b53f09353b5
taolin <tanlin2013@gmail.com>
2025-03-20 18:50:40 +0800

    chore: update job script

--- scripts/xy_scars.py
+++ scripts/xy_scars.py
@@ -4,7 +4,6 @@ from threading import Lock
 import numpy as np
 import pandas as pd
 import ray
-from scipy.sparse.csgraph import connected_components
 
 from qlinks import logger
 from qlinks.distributed import map_on_ray
@@ -21,9 +20,12 @@ def setup_storage():
                 "coup_j",
                 "coup_h",
                 "coup_d",
+                "coup_j3",
                 "periodic",
+                "eval",
+                "kin",
+                "pot",
                 "sz",
-                "nullity",
             ]
         )
         df.to_csv(csv_file, index=False)
@@ -34,64 +36,28 @@ def task_wrapper(args):
     return task(*args)
 
 
-def task(n, coup_j, coup_h, coup_d, periodic):
-    model = Spin1XYModel(n, coup_j, coup_h, coup_d, periodic)
+def task(n, coup_j, coup_h, coup_d, coup_j3, periodic):
+    model = Spin1XYModel(n, coup_j, coup_h, coup_d, coup_j3, periodic)
     mat = model.hamiltonian.toarray()
-    n_components, labels = connected_components(
-        mat, directed=False, connection="weak", return_labels=True
-    )
-    for i in range(n_components):
-        mask = (labels == i)  # fmt: skip
-        if np.count_nonzero(mask) > 1:
-            sub_mat = mat[np.ix_(mask, mask)]
-            (sz,) = np.unique(np.diag(sub_mat)) / coup_h
-            sub_mat -= coup_h * sz * np.eye(sub_mat.shape[0])
-            sub_mat /= coup_j
-            if n % 2 == 1 and periodic:
-                non_bipartite_graph(n, coup_j, coup_h, coup_d, periodic, sz, sub_mat)
-            else:
-                two_step_mat = sub_mat @ sub_mat
-                bipartite_graph(n, coup_j, coup_h, coup_d, periodic, sz, two_step_mat)
-    return
+    evals, evecs = np.linalg.eigh(mat)
 
+    sz = Spin1XYModel(n, coup_j, 1, 0, coup_j3, periodic).potential_term
 
-def bipartite_graph(n, coup_j, coup_h, coup_d, periodic, sz, two_step_mat):
-    n_components, labels = connected_components(
-        two_step_mat, directed=False, connection="weak", return_labels=True
-    )
-    for i in range(n_components):
-        mask = (labels == i)  # fmt: skip
-        if np.count_nonzero(mask) > 1:
-            sub_sub_mat = two_step_mat[np.ix_(mask, mask)]
-            _df = pd.DataFrame.from_dict(
-                {
-                    "n": [n],
-                    "coup_j": [coup_j],
-                    "coup_h": [coup_h],
-                    "coup_d": [coup_d],
-                    "periodic": [periodic],
-                    "sz": [sz],
-                    "nullity": [sub_sub_mat.shape[0] - np.linalg.matrix_rank(sub_sub_mat)],
-                }
-            )
-            with Lock():
-                _df.to_csv(csv_file, mode="a", index=False, header=False)
-            logger.info("\n\t" + _df.to_string(index=False).replace("\n", "\n\t"))
-    return
-
-
-def non_bipartite_graph(n, coup_j, coup_h, coup_d, periodic, sz, sub_mat):
-    _df = pd.DataFrame.from_dict(
+    evecs_df = pd.DataFrame.from_dict(
         {
-            "n": [n],
-            "coup_j": [coup_j],
-            "coup_h": [coup_h],
-            "coup_d": [coup_d],
-            "periodic": [periodic],
-            "sz": [sz],
-            "nullity": [sub_mat.shape[0] - np.linalg.matrix_rank(sub_mat)],
+            "n": n,
+            "coup_j": coup_j,
+            "coup_h": coup_h,
+            "coup_d": coup_d,
+            "coup_j3": coup_j3,
+            "periodic": periodic,
+            "eval": evals,
+            "kin": [(evec.T @ model.kinetic_term @ evec).item() for evec in evecs.T],
+            "pot": [(evec.T @ model.potential_term @ evec).item() for evec in evecs.T],
+            "sz": [(evec.T @ sz @ evec).item() for evec in evecs.T],
         }
     )
+    _df = evecs_df[evecs_df["kin"].abs() < 1e-12].copy()
     with Lock():
         _df.to_csv(csv_file, mode="a", index=False, header=False)
     logger.info("\n\t" + _df.to_string(index=False).replace("\n", "\n\t"))
@@ -99,11 +65,11 @@ def non_bipartite_graph(n, coup_j, coup_h, coup_d, periodic, sz, sub_mat):
 
 
 if __name__ == "__main__":
-    coup_j, coup_h, coup_d = (1, 1, 0)
+    coup_j, coup_h, coup_d, coup_j3 = 2 * (np.random.rand(4) - 0.5)
     # fmt: off
     inputs = [
-        (n, coup_j, coup_h, coup_d, periodic)
-        for n in range(3, 4)
+        (n, coup_j, coup_h, coup_d, coup_j3, periodic)
+        for n in range(4, 5)
         for periodic in [True, False]
     ]
     # fmt: on
