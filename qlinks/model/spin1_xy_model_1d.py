1effd1a7ea9b7443ac70005fac28b3c23d6e9098
taolin <tanlin2013@gmail.com>
2026-04-12 15:12:59 +0800

    feat: add exact scar constructors

--- qlinks/model/spin1_xy_model_1d.py
+++ qlinks/model/spin1_xy_model_1d.py
@@ -60,7 +60,7 @@ class Spin1XYModel:
         s_z, idty = sop.s_z, sop.idty
         for site in range(self.n):
             self._hamiltonian += self.coup_h * kron([s_z, *[idty] * (self.n - 1)], shift=site)
-            self._hamiltonian += self.coup_d * kron([s_z**2, *[idty] * (self.n - 1)], shift=site)
+            self._hamiltonian += self.coup_d * kron([s_z @ s_z, *[idty] * (self.n - 1)], shift=site)
 
     def _build_h3_term(self):
         sop = SpinOperators(1)
@@ -103,3 +103,64 @@ class Spin1XYModel:
         return pd.DataFrame.from_dict(
             {i: j for i, j in enumerate(product([1, 0, -1], repeat=self.n))}, orient="index"
         )
+
+    def vacuum_state(self) -> sp.sparray:
+        state = sp.csr_array((3**self.n, 1), dtype=int)
+        state[-1, 0] = 1
+        return state
+
+    def sn_tower(self, m: int) -> sp.sparray:
+        def j_raise_operator():
+            sop = SpinOperators(1)
+            s_plus, idty = sop.s_plus, sop.idty
+            _j_op = sp.csr_array((3**self.n, 3**self.n), dtype=float)
+            for site in range(self.n):
+                _j_op += (-1) ** site * kron([s_plus @ s_plus, *[idty] * (self.n - 1)], shift=site)
+            return 0.5 * _j_op
+
+        if m < 0 or m > self.n:
+            raise ValueError("The value of m should be m = 0, 1, ..., n.")
+        if m == 0:
+            return self.vacuum_state()
+        j_op = j_raise_operator()
+        state = sp.linalg.matrix_power(j_op, m) @ self.vacuum_state()
+        return state / sp.linalg.norm(state)
+
+    def sn_prime_tower(self, m: int) -> sp.sparray:
+        if not self.periodic and self.n % 2 != 0:
+            raise ValueError("The system should be periodic or have even number of sites.")
+        if m < 0 or m > self.n:
+            raise ValueError("The value of m should be m = 0, 1, ..., n.")
+
+        return
+
+    def sn_dprime_tower(self, m: int, parity: int = 0) -> sp.sparray:
+        sop = SpinOperators(1)
+        s_plus, idty = sop.s_plus, sop.idty
+
+        def j_raise_operator(idx):
+            _op = sp.csr_array((3**self.n, 3**self.n), dtype=float)
+            for site in range(self.n):
+                if site < idx:
+                    _op += (-1) ** site * kron([s_plus @ s_plus, *[idty] * (self.n - 1)], shift=site)
+                else:
+                    _op += (-1) ** (site - 1) * kron([s_plus @ s_plus, *[idty] * (self.n - 1)], shift=site)
+            return _op
+
+        if not ((self.periodic and self.n % 4 == 0) or (not self.periodic and self.n % 2 == 1)):
+            raise ValueError("The system size and boundary conditions can not support this state.")
+        if parity not in [0, 1]:
+            raise ValueError("Can only define on even or odd sites.")
+        if m < 0 or m > self.n:
+            raise ValueError("The value of m should be m = 0, 1, ..., n.")
+
+        state = sp.csr_array((3**self.n, 1), dtype=float)
+        for site in range(parity, self.n, 2):
+            j_op = j_raise_operator(site)
+            state += (
+                np.exp(0.5j * np.pi * (site + parity))
+                * kron([s_plus, *[idty] * (self.n - 1)], shift=site)
+                @ sp.linalg.matrix_power(j_op, m)
+                @ self.vacuum_state()
+            )
+        return state / sp.linalg.norm(state)
