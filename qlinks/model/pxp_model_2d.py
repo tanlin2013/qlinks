f6c63dfb7cd5287fc30d9be7dc39fa8349901ade
taolin <tanlin2013@gmail.com>
2026-04-23 16:07:15 +0800

    feat: init abstract class for 2d pxp model

---
+++ qlinks/model/pxp_model_2d.py
@@ -0,0 +1,21 @@
+from __future__ import annotations
+
+from dataclasses import dataclass, field
+from typing import Tuple
+
+import numpy as np
+import numpy.typing as npt
+import scipy.sparse as sp
+from ortools.sat.python import cp_model
+
+
+@dataclass(slots=True)
+class CpModel:
+    shape: Tuple[int, ...]
+
+
+@dataclass(slots=True)
+class PXPModel2D:
+    shape: Tuple[int, int]
+    periodic: bool = False
+
